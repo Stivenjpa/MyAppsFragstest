e58baf90394b33244e5076243fec3b4ac6bc534f
EstivenJaramilloGomez <149891748+Stivenjpa@users.noreply.github.com>
2025-04-02 21:58:51 -0500

    fragment_test_layout

--- app/src/main/java/com/example/myappsfragstest/MainActivity.kt
+++ app/src/main/java/com/example/myappsfragstest/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.myappsfragstest
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.myappsfragstest.test.TestFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
         enableEdgeToEdge()
         supportActionBar?.hide()
+
+        if(savedInstanceState == null){
+            val bundle = bundleOf()
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<TestFragment>(R.layout.fragment_test,args = bundle)
+            }
+        }
 
     }
 }
--- app/src/main/java/com/example/myappsfragstest/test/TestFragment.kt
+++ app/src/main/java/com/example/myappsfragstest/test/TestFragment.kt
@@ -7,26 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.myappsfragstest.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TestFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class TestFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var name: String? = null
+    private var num1:Float = 0.0f
+    private var num2:Float = 0.0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            name = it.getString(NOMBRE_BUNDLE)
+            num1 = it.getFloat(NUM1_BUNDLE)
+            num2 = it.getFloat(NUM2_BUNDLE)
         }
     }
 
@@ -34,26 +29,23 @@ class TestFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_test, container, false)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TestFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+        private const val NOMBRE_BUNDLE = "param1"
+        private const val NUM1_BUNDLE = "param2"
+        private const val NUM2_BUNDLE = "param3"
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String, param2: Float,param3:Float) =
             TestFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(NOMBRE_BUNDLE, param1)
+                    putFloat(NUM1_BUNDLE, param2)
+                    putFloat(NUM2_BUNDLE, param3)
+
                 }
             }
     }
